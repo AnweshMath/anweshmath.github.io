06d212078c171cf359354796ffb406d24123e1be
gperm <gpermweb@gmail.com>
2024-08-20 19:29:43 +0530

    Linked

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 function web() {
-	window.location.assign("index.html");
+	window.open("index.html", "_blank");
 }
-
+function c1() {
+	window.open("Class-1.html", "_blank");
+}
+function c2() {
+	window.open("Class-2.html", "_blank");
+}
+function c3() {
+	window.open("Class-3.html", "_blank");
+}
+function c4() {
+	window.open("Class-4.html", "_blank");
+}
+function c5() {
+	window.open("Class-5.html", "_blank");
+}
+function c6() {
+	window.open("Class-6.html", "_blank");
+}
+function c7() {
+	window.open("Class-7.html", "_blank");
+}
+function c8() {
+	window.open("Class-8.html", "_blank");
+}
+function c9() {
+	window.open("Class-9.html", "_blank");
+}
+function c10() {
+	window.open("Class-10.html", "_blank");
+}
+function c11() {
+	window.open("Class-11.html", "_blank");
+}
+function c12() {
+	window.open("Class-12.html", "_blank");
+}
 function contact() {
-	window.location.assign("https://forms.gle/ccQvEqtB1R6RaouM8");
+	window.open("https://forms.gle/ccQvEqtB1R6RaouM8.html", "_blank");
 }
 function Navtop() {
 	document.getElementById("Nav").style.position = "sticky";
