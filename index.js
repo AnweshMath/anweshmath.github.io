fdccc614a3adedf903bf9eb2384dc49d4816dd3d
gperm <gpermweb@gmail.com>
2024-08-20 22:56:55 +0530

    7

--- index.js
+++ index.js
@@ -38,8 +38,66 @@ function c12() {
 	window.open("Class-12.html", "_blank");
 }
 function contact() {
-	window.open("https://forms.gle/ccQvEqtB1R6RaouM8.html", "_blank");
+	window.open("https://forms.gle/DaBzutdB1dVWo7CWA", "_blank");
 }
+function Ch7and1() {
+	window.open("7ch1.html", "_blank");
+}
+function Ch7and2() {
+	window.open("7ch2.html", "_blank");
+}
+function Ch7and3() {
+	window.open("7ch3.html", "_blank");
+}
+function Ch7and4() {
+	window.open("7ch4.html", "_blank");
+}
+function Ch7and5() {
+	window.open("7ch5.html", "_blank");
+}
+function Ch7and6() {
+	window.open("7ch6.html", "_blank");
+}
+function Ch7and7() {
+	window.open("7ch7.html", "_blank");
+}
+function Ch7and8() {
+	window.open("7ch8.html", "_blank");
+}
+function Ch7and9() {
+	window.open("7ch9.html", "_blank");
+}
+function Ch7and10() {
+	window.open("7ch10.html", "_blank");
+}
+function Ch7and11() {
+	window.open("7ch11.html", "_blank");
+}
+function Ch7and12() {
+	window.open("7ch12.html", "_blank");
+}
+function Ch7and13() {
+	window.open("7ch13.html", "_blank");
+}
+function Ch7and14() {
+	window.open("7ch14.html", "_blank");
+}
+function Ch7and15() {
+	window.open("7ch15.html", "_blank");
+}
+function Ch7and16() {
+	window.open("7ch16.html", "_blank");
+}
+function Ch7and17() {
+	window.open("7ch17.html", "_blank");
+}
+function Ch7and18() {
+	window.open("7ch18.html", "_blank");
+}
+function Ch7and19() {
+	window.open("7ch19.html", "_blank");
+}
+
 function Navtop() {
 	document.getElementById("Nav").style.position = "sticky";
 	document.getElementById("Nav").style.position = " -webkit-sticky";
